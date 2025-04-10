2079866ca5944a0804916779c15658ef2d99eaec
IgorP25 <igor@zimp.com.au>
2025-04-11 05:01:03 +1000

    Added command handler

--- main.go
+++ main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/IgorP25/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
+		log.Fatalf("error reading config: %v", err)
 	}
-	cfg.SetUser("igor")
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		return
+	programState := &state{
+		cfg: &cfg,
 	}
 
-	fmt.Println(cfg)
+	commands := commands{
+		registeredCommands: map[string]func(*state, command) error{},
+	}
+
+	commands.register("login", handlerLogin)
+
+	args := os.Args
+
+	if len(args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := args[1]
+	cmdArgs := args[2:]
+
+	err = commands.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
